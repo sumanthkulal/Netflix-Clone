054f6110711f98fba5fd19a8219a836b6d9df75d
sumanth_kulal <sumanthkulal2005@gmail.com>
2025-04-13 23:04:32 +0530

    implemented navigation bar

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
+
+const Home = () => {
+  return (
+    <div className='home'>
+       <Navbar/>
+       <Footer/>
+    </div>
+  )
+}
+
+export default Home
